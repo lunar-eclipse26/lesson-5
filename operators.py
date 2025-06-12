f6ffd61fd27b992d7d3f0425611145fce9f7eb4c
lunar_eclipse <sleepysmorz@gmail.com>
2025-06-12 17:45:38 +1200

    operators

---
+++ operators.py
@@ -0,0 +1,8 @@
+chid1 = 5
+chid2 = 7
+chid3 = 743
+chid9756 = 2
+sum = chid1+chid2+chid3+chid9756
+print("the age of all the chids combined is", sum)
+average = sum/4
+print("the average age of all the chids is", average)
